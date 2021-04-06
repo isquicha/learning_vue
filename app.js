0afd6ec981abe5e777104843fba46d1b62c3e800
Andre Vicente <isquicha@gmail.com>
2021-04-06 02:27:40 -0300

    feat: Add random user generator

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const app = Vue.createApp({
             gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
         };
+    },
+    methods: {
+        async getUser() {
+            const response = await fetch('https://randomuser.me/api');
+            const { results } = await response.json();
+            const randomPerson = results[0];
+
+            this.firstName = randomPerson.name.first;
+            this.lastName = randomPerson.name.last;
+            this.email = randomPerson.email;
+            this.gender = randomPerson.gender;
+            this.picture = randomPerson.picture.large;
+        }
     }
 });
 
